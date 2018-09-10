f630d65228ad2e00000a5c3c9770957743317201
Koji Osugi <koji097@gmail.com>
2018-09-09 21:30:19 -0300

    first commit

---
+++ src/main/java/com/vamos/VamosApplication.java
@@ -0,0 +1,12 @@
+package com.vamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VamosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VamosApplication.class, args);
+	}
+}
---
+++ src/main/java/com/vamos/services/DBService.java
@@ -0,0 +1,12 @@
+package com.vamos.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+	
+	public void instantiateDataBase() {
+		
+	}
+
+}
